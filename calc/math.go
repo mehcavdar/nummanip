767a8c7a85a68401f2423bb66b3686d07357f8dd
mehmet cavdar <mehcav@gmail.comgit config --global user.email mehcav@gmail.com>
2021-12-08 20:47:57 +0300

    yeni versiyon

--- calc/math.go
+++ calc/math.go
@@ -1,5 +1,12 @@
 package calc
 
-func Add(a int, b int) int {
-	return a + b
+func Add(numbers ...int) int {
+
+	sum := 0
+
+	for _, num := range numbers {
+
+		sum = sum + num
+	}
+	return sum
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/mehcavdar/nummanip/v2
+module github.com/mehcavdar/nummanip
 
 go 1.17
